6444bf028c5e0cf28eef5dd8317bb75bddecef93
Jesus Robles <jrobles0302@gmail.com>
2021-02-26 23:17:31 -0500

    Flix - Part 2

---
+++ flix/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  flix
+//
+//  Created by Jesus Robles on 2/26/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
